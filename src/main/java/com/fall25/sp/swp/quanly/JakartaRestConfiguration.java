5d5a38f49ea44044be49e883bc9de4bba0922dd0
nguyenquangduy <151936156+Subzerozzz@users.noreply.github.com>
2025-09-18 23:40:04 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/fall25/sp/swp/quanly/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.fall25.sp.swp.quanly;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/fall25/sp/swp/quanly/config/GlobalConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fall25.sp.swp.quanly.config;
+
+/**
+ *
+ * @author Dell
+ */
+public class GlobalConfig {
+    
+}
