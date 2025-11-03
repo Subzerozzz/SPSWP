7ae011a37a4feb34491002a5f63eca09fa18b810
Khoa-Pham-Van <khoapvhe181066@fpt.edu.vn>
2025-11-03 19:08:48 +0700

    view club home

--- src/main/java/com/fall25/sp/swp/quanly/controller/home/ClubsForHome.java
+++ src/main/java/com/fall25/sp/swp/quanly/controller/home/ClubsForHome.java
@@ -1,7 +1,9 @@
 package com.fall25.sp.swp.quanly.controller.home;
 
 import com.fall25.sp.swp.quanly.dal.implement.ClubDAO;
+import com.fall25.sp.swp.quanly.dal.implement.EventDAO;
 import com.fall25.sp.swp.quanly.entity.Club;
+import com.fall25.sp.swp.quanly.entity.Event;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,11 +16,15 @@ import java.util.stream.Collectors;
 
 @WebServlet("/clubsForHome")
 public class ClubsForHome extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
         switch (action) {
             case "viewClubs":
                 viewClubs(request, response);
+                break;
+            case "viewDetail":
+                viewDetail(request, response);
                 break;
             default:
                 break;
@@ -30,5 +36,27 @@ public class ClubsForHome extends HttpServlet {
         System.out.println("clubList: " + clubList);
         request.setAttribute("clubList", clubList);
         request.getRequestDispatcher("view/guest/page/listClub.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse reqs) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        switch (action) {
+            case "viewDetail":
+                viewDetail(req, reqs);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    protected void viewDetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer clubId = Integer.parseInt(request.getParameter("clubId"));
+        Club club = new ClubDAO().findById(clubId);
+        List<Event> eventList = new EventDAO().findByClubId(clubId).stream().filter(event -> event.getStatus().equals("active")).collect(Collectors.toList());
+
+        System.out.println("eventList: " + eventList);
+        request.setAttribute("eventList", eventList);
+        request.setAttribute("club", club);
+        request.getRequestDispatcher("view/guest/page/detailClub.jsp").forward(request, response);
     }
 }
